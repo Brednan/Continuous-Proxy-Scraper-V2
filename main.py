46d71a7f7bfb411d470cf41f4dfc7cf1c3c75c69
brednan <brendanshauncaldwell@gmail.com>
2023-08-28 12:58:18 -0700

    Added .gitignore

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from proxy_scraper import *
 from database import ProxyDatabase
-import os
 from utilities import *
 import time
 
 
 if __name__ == '__main__':
-    env_vars = os.environ
-
-    with open('./mysql.txt', 'r') as f:
-        content = f.read().split('\n')
-        mysql_ip = content[0]
-        mysql_username = content[1]
+    mysql_ip, mysql_username, mysql_password = get_database_credentials()
 
     while True:
         print(f'{get_date_time_str()} - Starting proxy scrape')
 
         proxies = scrape_proxies()
-        database = ProxyDatabase(mysql_ip, mysql_username, env_vars.get('mysql_pass'))
+        database = ProxyDatabase(mysql_ip, mysql_username, mysql_password)
         print(f'{get_date_time_str()} - Finished proxy scrape')
 
         database.add_proxies(proxies)
--- utilities.py
+++ utilities.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 
 
@@ -6,3 +7,13 @@ def get_date_time_str():
 
     return f'{current_date_time.year}/{current_date_time.month:02d}/{current_date_time.day:02d} {current_date_time.hour:02d}:' \
            f'{current_date_time.minute:02d}'
+
+
+def get_database_credentials() -> tuple:
+    with open('./mysql.txt', 'r') as f:
+        content = f.read().split('\n')
+        mysql_ip = content[0]
+        mysql_username = content[1]
+
+    env_vars = os.environ
+    return mysql_ip, mysql_username, env_vars.get('mysql_pass')
